fd31668836a1c261db275129b5d818a894d4e0c2
Jainandhini <jainandhinis@gmail.com>
2017-11-30 10:50:04 -0600

    ex2 upload from pycharm to git

---
+++ w3resource-1/com/pybasics/ex2.py
@@ -0,0 +1,8 @@
+#print version of python
+import sys
+print(sys.version)
+print(sys.version_info.major)
+print(sys.version_info.minor)
+print(sys.version_info.micro)
+print(sys.version_info.releaselevel)
+print(sys.version_info.serial)
